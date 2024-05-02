4e8fc13c40a8a3feeee2e3559d0354acb3fd1dce
Adrián García <adriangargar99@gmail.com>
2024-05-02 19:29:42 +0200

    extract methods

--- src/Item.ts
+++ src/Item.ts
@@ -30,29 +30,28 @@ export class Item {
       }
     } else {
       if (this.quality > 0) {
-        if (this.name != "Sulfuras, Hand of Ragnaros") {
-          this.decrementQualityByOne()
-        }
+        if (this.name == "Sulfuras, Hand of Ragnaros") return
+        this.decrementQualityByOne()
       }
     }
     if (this.name != "Sulfuras, Hand of Ragnaros") {
       this.sellIn = this.sellIn - 1
     }
     if (this.sellIn < 0) {
-      if (this.name != "Aged Brie") {
-        if (this.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (this.quality > 0) {
-            if (this.name != "Sulfuras, Hand of Ragnaros") {
-              this.decrementQualityByOne()
-            }
-          }
-        } else {
-          this.decrementQualityToZero()
-        }
-      } else {
+      if (this.name == "Aged Brie") {
         if (this.isQualityBelow(50)) {
           this.incrementQualityByOne()
+          return
         }
+        return
+      }
+      if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
+        this.decrementQualityToZero()
+        return
+      }
+      if (this.quality > 0) {
+        if (this.name == "Sulfuras, Hand of Ragnaros") return
+        this.decrementQualityByOne()
       }
     }
   }
