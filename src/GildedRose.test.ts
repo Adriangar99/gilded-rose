cdf60190da54141cc43e2e63a52338af723d2e9f
Alejandro Gonzalez <alejandro.gonzalez@dezzai.com>
2024-05-02 18:01:02 +0100

    creates golden master test

---
+++ src/GildedRose.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest"
+import { GildedRose, Item } from "./GildedRose.js"
+
+describe("GildedRose", () => {
+  const itemNames = ["Apple", "Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"]
+  for (const name of itemNames) {
+    for (let sellIn = -2; sellIn <= 10; sellIn++) {
+      for (let quality = -2; quality <= 51; quality++) {
+        const item = new Item(name, sellIn, quality)
+
+        it(JSON.stringify(item), () => {
+          const sut = new GildedRose([item])
+          const items = sut.updateQuality()
+
+          expect(items[0]).toMatchSnapshot()
+        })
+      }
+    }
+  }
+})
